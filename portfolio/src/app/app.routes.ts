5d4471d1d9934e4d896dd12f05c37917b0537b32
MatiasTanoni <matitanoni@gmail.com>
2026-02-19 10:24:57 -0300

    navbar con los componentes basicos

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -11,6 +11,18 @@ export const routes: Routes = [
         loadComponent: () => import('./pages/home/home').then(m => m.Home)
     },
     {
+        path: 'projects',
+        loadComponent: () => import('./pages/projects/projects').then(m => m.Projects)
+    },
+    {
+        path: 'about',
+        loadComponent: () => import('./pages/about/about').then(m => m.About)
+    },
+    {
+        path: 'contact',
+        loadComponent: () => import('./pages/contact/contact').then(m => m.Contact)
+    },
+    {
         path: '**',
         loadComponent: () => import('./pages/home/home').then(m => m.Home)
     }
