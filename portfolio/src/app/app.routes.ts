7826970f6ad0c446dfa07257e47edbdd69ae8dfa
MatiasTanoni <matitanoni@gmail.com>
2026-02-19 09:49:28 -0300

    arrancando

---
+++ portfolio/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home)
+    }
+];
