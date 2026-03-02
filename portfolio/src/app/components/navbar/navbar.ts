3fd0e9e1d8008e745bf54f05a6e4c12d9ecb58de
MatiasTanoni <matitanoni@gmail.com>
2026-03-02 18:58:03 -0300

    navbar responsive

--- portfolio/src/app/components/navbar/navbar.ts
+++ portfolio/src/app/components/navbar/navbar.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgClass],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+  isOpen = false;
 
+  menu = [
+    { label: 'Inicio', link: '/home' },
+    { label: 'Proyectos', link: '/projects' },
+    { label: 'Educación', link: '/education' },
+    { label: 'Experiencia', link: '/experience' },
+    { label: 'Contacto', link: '/contact' }
+  ];
 }
