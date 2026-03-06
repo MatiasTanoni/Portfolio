f6a73c98dba947e87467aa61d970747f661019cc
MatiasTanoni <matitanoni@gmail.com>
2026-03-06 17:33:33 -0300

    MSN

--- portfolio/src/app/components/navbar/navbar.ts
+++ portfolio/src/app/components/navbar/navbar.ts
@@ -12,7 +12,7 @@ export class Navbar {
   isOpen = false;
 
   menu = [
-    { label: 'Inicio', link: '/' },
+    { label: 'Inicio', link: '/home' },
     { label: 'Proyectos', link: '/projects' },
     { label: 'Educación', link: '/education' },
     { label: 'Experiencia', link: '/experience' },
